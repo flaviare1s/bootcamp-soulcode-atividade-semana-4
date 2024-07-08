46668d377abae995782252100035aa2878fb884e
flaviare1s <flaviarster@gmail.com>
2024-07-07 21:55:19 -0300

    Coloquei mais informações no footer

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -3,6 +3,10 @@ import './Rodape.css'
 const Rodape = () => {
   return (
     <footer>
+      <h4>Adote um Pet</h4>
+      <p>Email: contato@adoteumpet.org</p>
+      <p>Telefone: (85) 1234-5678</p>
+      <p>Endereço: Rua dos Pets, 123, Fortaleza - CE</p>
       <p>©️ 2024 - Todos os direitos reservados</p>
     </footer>
   )
